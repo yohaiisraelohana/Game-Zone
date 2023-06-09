d210672af1f1037d7493f8071f5529346c640a19
yohai <ohanayohaiisrael@gmail.com>
2023-06-09 10:02:28 +0300

    ok

---
+++ frontend/src/components/games/gamesCollection/gameCollection.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+export default function gameCollection() {
+
+  return (
+    <div>
+      <div className="">
+        <div className="memoryGame">
+          
+        </div>
+      </div>
+    </div>
+  )
+}
