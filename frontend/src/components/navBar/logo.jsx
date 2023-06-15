f74393d6d6af82fb98e93436648d1533f22e5883
Noam Avitan <noamavitan159753@gmail.com>
2023-06-15 20:10:29 +0300

    k

--- frontend/src/components/navBar/logo.jsx
+++ frontend/src/components/navBar/logo.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import './logo.css'
+import React from 'react';
+import './logo.css';
+import { useNavigate } from 'react-router-dom/dist/umd/react-router-dom.development';
+
 export default function Logo() {
+  const nav = useNavigate();
   return (
-    <div className="logo-container">
-        <div className="logo-image"></div>
-        <p className="logo-name">Game Zone</p>
-    </div>
-  )
+    <button onClick={() => nav('/')} className="logo-container" style={{ background: 'none', border: 'none', outline: 'none' }}>
+      <div className="logo-image"></div>
+      <p className="logo-name">Game Zone</p>
+    </button>
+  );
 }
