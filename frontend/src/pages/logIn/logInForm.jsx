5c27d8b450943659b58516f668a352961615f63b
Noam Avitan <noamavitan159753@gmail.com>
2023-06-16 17:16:30 +0300

    UsersForm

--- frontend/src/pages/logIn/logInForm.jsx
+++ frontend/src/pages/logIn/logInForm.jsx
@@ -1,69 +1,72 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 //style
-import './logInForm.css'
-import SignUpForm from '../signUp/signUpForm'
-import {useForm} from 'react-hook-form';
-import { Link } from 'react-router-dom';
+import "./logInForm.css";
+import SignUpForm from "../signUp/signUpForm";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import useUser from "../../hooks/useUser";
 
+export default function LogInForm({ updateModal }) {
 
-
-export default function LogInForm({updateModal}) {
-  const { register,handleSubmit,reset,formState:{errors}} = useForm();
+  const { user, error, loading, loginUser } = useUser();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
   const [toggle, setToggle] = useState(false);
   const emailAlphabet = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
   const hundleLogIn = (data) => {
     console.log(data);
-    reset()
-  }
+    loginUser(data);
+    reset();
+  };
   return (
-    <div className='login-container'>
-      <div className='login-nav'>LogIn  
-        <button 
-          onClick={()=>updateModal(<SignUpForm updateModal={updateModal}/>)}
-          className='sign-up'>/SignUp</button>
-      </div> 
-      <form 
-        onSubmit={handleSubmit(hundleLogIn)}
-        className='login-form' >
-          <label >
-            Email
-          </label>
-          <input 
-            {...register("email", {
-              required: {value:true, message: "Email is Required..."},
-              minLength: { value: 8, message: "min 8 chars.." },
-              maxLength: { value: 40, message: "max 40 chars.." },
-              pattern: {
-                value: emailAlphabet,
-                message: "only alphabet allowed",
-              },
-            })}
-            placeholder='yourEmail@...'
-            type="text" />
-            {errors.email && <p>{errors.email.message}</p>}
-          <label >
-            Password
-          </label>
-          <input 
-            {...register("password", {
-              required: { value: true, message: "Password is Required..." },
-              minLength: { value: 5, message: "min 5 chars.." },
-              maxLength: { value: 40, message: "max 40 chars.." },
-            })}
-            type={!toggle?'Password':'text'}
-            placeholder="Type Password.." />
-            {errors.password && <p>{errors.password.message}</p>}
-            <div>
-              <p>{}</p>
-            </div>
-          <Link className='forgot-password'>
-            Forgot your password ?
-          </Link>
-          <button className='login-button'>
-            LogIn
-          </button>
+    <div className="login-container">
+      <div className="login-nav">
+        LogIn
+        <button
+          onClick={() => updateModal(<SignUpForm updateModal={updateModal} />)}
+          className="sign-up"
+        >
+          /SignUp
+        </button>
+      </div>
+      <form onSubmit={handleSubmit(hundleLogIn)} className="login-form">
+        <label>Email</label>
+        <input
+          {...register("email", {
+            required: { value: true, message: "Email is Required..." },
+            minLength: { value: 8, message: "min 8 chars.." },
+            maxLength: { value: 40, message: "max 40 chars.." },
+            pattern: {
+              value: emailAlphabet,
+              message: "only alphabet allowed",
+            },
+          })}
+          placeholder="yourEmail@..."
+          type="text"
+        />
+        {errors.email && <p>{errors.email.message}</p>}
+        <label>Password</label>
+        <input
+          {...register("password", {
+            required: { value: true, message: "Password is Required..." },
+            minLength: { value: 5, message: "min 5 chars.." },
+            maxLength: { value: 40, message: "max 40 chars.." },
+          })}
+          type={!toggle ? "Password" : "text"}
+          placeholder="Type Password.."
+        />
+        {errors.password && <p>{errors.password.message}</p>}
+        <div>
+          <p>{error}</p>
+        </div>
+        <Link className="forgot-password">Forgot your password ?</Link>
+        <button className="login-button">LogIn</button>
       </form>
     </div>
-  )
+  );
 }
--- frontend/src/pages/signUp/signUpForm.jsx
+++ frontend/src/pages/signUp/signUpForm.jsx
@@ -1,50 +1,81 @@
-import React, { useState } from 'react'
-import { useForm } from 'react-hook-form'
-import LogInForm from '../logIn/logInForm';
-import './signUpForm.css'
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import LogInForm from "../logIn/logInForm";
+import "./signUpForm.css";
+import useUser from "../../hooks/useUser";
 
-export default function SignUpForm({updateModal}) {
-    const {register,handleSubmit,reset,formState:{errors}} = useForm();
-    const hundleSignUp = (data) => {
-        console.log(data);
-        reset();
-      }
-      
+export default function SignUpForm({ updateModal }) {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const {user,error,loading,signUpUser} = useUser();
+  const nameRegex = /^[a-zA-Z ]+$/;
+  const emailAlphabet = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+  const [toggle, setToggle] = useState(false);
+
+  const hundleSignUp = (data) => {
+    console.log(data);
+    signUpUser(data);
+    reset();
+  };
+
   return (
-    <div className='signUp-container'>
-    <div className='signUp-nav'>SignUp 
-      <button 
-        onClick={()=>updateModal(<LogInForm updateModal={updateModal}/>)}
-        className='log-in'>/LogIn</button>
-    </div> 
-    <form 
-      onSubmit={handleSubmit(hundleSignUp)}
-      className='signUp-form' >
-        <label >
-          Name
-        </label>
-        <input 
-          {...register("name")}
-          placeholder='your name here'
-          type="text" />
-        <label >
-          Email
-        </label>
-        <input 
-          {...register("email")}
-          placeholder='yourEmail@...'
-          type="text" />
-        <label >
-          Password
-        </label>
-        <input 
-          {...register("password")}
-          placeholder='****'
-          type="text" />
-        <button className='signUp-button'>
-          SignUp
+    <div className="signUp-container">
+      <div className="signUp-nav">
+        SignUp
+        <button
+          onClick={() => updateModal(<LogInForm updateModal={updateModal} />)}
+          className="log-in"
+        >
+          /LogIn
         </button>
-    </form>
-  </div>
-  )
+      </div>
+      <form onSubmit={handleSubmit(hundleSignUp)} className="signUp-form">
+        <label>Name</label>
+        <input
+          {...register("name", {
+            required: { value: true, message: "name is Required..." },
+            minLength: { value: 10, message: "min 6 chars.." },
+            maxLength: { value: 30, message: "max 40 chars.." },
+            pattern: {
+              value: nameRegex,
+              message: "only alphabet allowed",
+            },
+          })}
+          placeholder="Type Your Name.."
+          type="text"
+        />
+        {errors.name && <p>{errors.name.message}</p>}
+        <label>Email</label>
+        <input
+          {...register("email", {
+            required: { value: true, message: "Email is Required..." },
+            minLength: { value: 10, message: "min 8 chars.." },
+            maxLength: { value: 30, message: "max 40 chars.." },
+            pattern: {
+              value: emailAlphabet,
+              message: "only alphabet allowed",
+            },
+          })}
+          placeholder="yourEmail@..."
+          type="text"
+        />
+        {errors.email && <p>{errors.email.message}</p>}
+        <label>Password</label>
+        <input
+          {...register("password", {
+            required: { value: true, message: "Password is Required..." },
+            minLength: { value: 5, message: "min 5 chars.." },
+            maxLength: { value: 40, message: "max 40 chars.." },
+          })}
+          type={!toggle ? "Password" : "text"}
+          placeholder="Type Password.."
+        />
+        <button className="signUp-button">SignUp</button>
+      </form>
+    </div>
+  );
 }
