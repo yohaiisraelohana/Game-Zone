7491065474811f88bba3c53e6a45a581140e569a
Noam Avitan <noamavitan159753@gmail.com>
2023-06-21 13:19:49 +0300

    ok

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,56 @@
+// Server-side code (index.js)
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const http = require('http');
+const { Server } = require('socket.io');
 
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
-//routes imports
-const userRoutes = require('./routes/user');
-const memoryGameRoutes = require('./routes/memoryGame');
-const sudokuRoutes = require('./routes/sudoku');
-
-//express app
+// Create an express app
 const app = express();
 
-//middleware
+// Middleware
 app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
+  origin: 'http://localhost:5173',
+  credentials: true
 }));
 app.use(cookieParser());
 app.use(express.json());
 app.use((req, res, next) => {
-    console.log(req.path, req.method);
-    next();
+  console.log(req.path, req.method);
+  next();
 });
 
-// routes
-app.use('/user', userRoutes);
-app.use('/memoryGame', memoryGameRoutes);
-app.use('/sudoku', sudokuRoutes);
-
-
-
-//connect to db
+// Connect to the database
 mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        //listen for requests
-        app.listen(process.env.PORT, () => {
-            console.log('Listening on port', process.env.PORT);
-        });
-    })
-    .catch((err) => {
-        console.log(err);
+  .then(() => {
+    // Create an HTTP server
+    const server = http.createServer(app);
+
+    // Initialize socket.io
+    const io = new Server(server, {
+      cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+      }
     });
+
+    // Handle socket.io events
+    io.on('connection', (socket) => {
+      console.log(`A user connected :${socket.id}`);
+
+      socket.on("send_Message", (data) => {
+        socket.broadcast.emit("received_message", data);
+      });
+    });
+
+    // Start the server
+    server.listen(process.env.PORT, () => {
+      console.log('Listening on port', process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- frontend/src/pages/homePage/home.jsx
+++ frontend/src/pages/homePage/home.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
-import './home.css'
+// Client-side code (Home.js)
+import React, { useEffect, useState } from 'react';
+import './home.css';
 import GameCollection from '../../components/games/gamesCollection/gameCollection';
+import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:3003');
+
 export default function Home() {
+    const [message,setMessage] = useState("");
+    const [messageReceived,setMessageReceived] = useState("");
+
+  const sendMessage = () => {
+    socket.emit("send_Message", { message: message });
+    setMessage("");
+  };
+  
+  useEffect(() => {
+    socket.on("received_message", (data) => {
+      setMessageReceived(data.message);
+    });
+  }, [socket]);
+
   return (
     <div className='home'>
-
+      <input type="text" onChange={(event) => {setMessage(event.target.value)}} />
+      <button onClick={sendMessage}>send message</button>
+      <h1>Message:</h1>
+      {messageReceived}
       <main>
         <GameCollection/>
       </main>
     </div>
-  )
+  );
 }
