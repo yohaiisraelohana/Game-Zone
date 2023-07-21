90c24ff40419db4024c3555d902aaa8f3a157504
yohai <ohanayohaiisrael@gmail.com>
2023-07-21 16:05:47 +0300

    sign up and login design

--- frontend/src/pages/signUp/signUpForm.jsx
+++ frontend/src/pages/signUp/signUpForm.jsx
@@ -42,56 +42,67 @@ export default function SignUpForm({ updateModal,closeModal }) {
         </button>
       </div>
       <form onSubmit={handleSubmit(hundleSignUp)} className="signUp-form">
-        {show_password
-          ? <AiOutlineEye className="eye-icon" onClick={() => setShowPassord(false)}   /> 
-          : <AiOutlineEyeInvisible className="eye-icon" onClick={()=>setShowPassord(true)}  /> }
-        <AiOutlineMail className="email-icon" />
-        <AiFillLock className="lock-icon" />
-        <AiOutlineUser className="user-icon"/>
+
+        
         <label>Name</label>
-        <input
-          {...register("name", {
-            required: { value: true, message: "Name is Required..." },
-            minLength: { value: 2, message: "min 2 chars.." },
-            maxLength: { value: 40, message: "max 40 chars.." },
-            pattern: {
-              value: nameRegex,
-              message: "only alphabet allowed",
-            },
-          })}
-          placeholder="Type Your Name.."
-          type="text"
-        />
+        <div className="input">
+        <AiOutlineUser className="user-icon"/>
+          <input
+            {...register("name", {
+              required: { value: true, message: "Name is Required..." },
+              minLength: { value: 2, message: "min 2 chars.." },
+              maxLength: { value: 40, message: "max 40 chars.." },
+              pattern: {
+                value: nameRegex,
+                message: "only alphabet allowed",
+              },
+            })}
+            placeholder="Type Your Name.."
+            type="text"
+          />
+        </div>
         <div className="error-client">
         {errors.name && <p>{errors.name.message}</p>}
         </div>
         <label>Email</label>
-        <input
-          {...register("email", {
-            required: { value: true, message: "Email is Required..." },
-            minLength: { value: 8, message: "min 8 chars.." },
-            maxLength: { value: 40, message: "max 40 chars.." },
-            pattern: {
-              value: emailAlphabet,
-              message: "only alphabet allowed",
-            },
-          })}
-          placeholder="yourEmail@..."
-          type="text"
-        />
+        <div className="input">
+        <AiOutlineMail className="email-icon" />
+          <input
+            {...register("email", {
+              required: { value: true, message: "Email is Required..." },
+              minLength: { value: 8, message: "min 8 chars.." },
+              maxLength: { value: 40, message: "max 40 chars.." },
+              pattern: {
+                value: emailAlphabet,
+                message: "only alphabet allowed",
+              },
+            })}
+            placeholder="yourEmail@..."
+            type="text"
+          />
+        </div>
+
         <div className="error-client">
         {errors.email && <p>{errors.email.message}</p>}
         </div>
         <label>Password</label>
-        <input
-          {...register("password", {
-            required: { value: true, message: "Password is Required..." },
-            minLength: { value: 5, message: "min 5 chars.." },
-            maxLength: { value: 40, message: "max 40 chars.." },
-          })}
-          type={show_password ? "text" : 'password'}
-          placeholder="Type Password.."
-        />
+        <div className="input">
+        {show_password
+          ? <AiOutlineEye className="eye-icon" onClick={() => setShowPassord(false)}   /> 
+          : <AiOutlineEyeInvisible className="eye-icon" onClick={()=>setShowPassord(true)}  /> }
+        
+        <AiFillLock className="lock-icon" />
+          <input
+            {...register("password", {
+              required: { value: true, message: "Password is Required..." },
+              minLength: { value: 5, message: "min 5 chars.." },
+              maxLength: { value: 40, message: "max 40 chars.." },
+            })}
+            type={show_password ? "text" : 'password'}
+            placeholder="Type Password.."
+          />
+        </div>
+
         <div className="error-client">
         {errors.password && <p>{errors.password.message}</p>}
         </div>
