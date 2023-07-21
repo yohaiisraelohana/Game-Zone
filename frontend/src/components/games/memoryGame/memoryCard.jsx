c300512fcf50f5c8c413ab1469407dbc6c44365b
yohai <ohanayohaiisrael@gmail.com>
2023-07-21 10:06:00 +0300

    design menagment

--- frontend/src/components/games/memoryGame/memoryCard.jsx
+++ frontend/src/components/games/memoryGame/memoryCard.jsx
@@ -7,7 +7,7 @@ export default function MemoryCard({card,choiseCard,flipped,disabled}) {
       
       <div 
       onClick={()=> !disabled && choiseCard(card)}
-      style={{backgroundImage:`url("https://res.cloudinary.com/dhojbnefp/image/upload/v1686320267/GameProject/memoryGame/9B55637C-4815-4169-947B-C0B1478146E9_mylsol.jpg")`}}
+      style={{backgroundImage:`url("https://res.cloudinary.com/dhojbnefp/image/upload/v1689870255/78185EDB-298E-4F9A-8F1D-3E60AAC192E2_1_201_a_on4uxp.jpg")`}}
       className="back">
       </div>
 
@@ -18,3 +18,12 @@ export default function MemoryCard({card,choiseCard,flipped,disabled}) {
   </div>
   )
 }
+
+
+/*
+      <div 
+      onClick={()=> !disabled && choiseCard(card)}
+      style={{backgroundImage:`url("https://res.cloudinary.com/dhojbnefp/image/upload/v1686320267/GameProject/memoryGame/9B55637C-4815-4169-947B-C0B1478146E9_mylsol.jpg")`}}
+      className="back">
+      </div>
+*/
