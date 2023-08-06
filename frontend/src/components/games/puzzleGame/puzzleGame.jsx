d0c5f2d3ffefec527888bc0bd987ddb4d31ed844
yohai <ohanayohaiisrael@gmail.com>
2023-08-06 23:24:32 +0300

    starting puzzle game

--- frontend/src/components/games/puzzleGame/puzzleGame.jsx
+++ frontend/src/components/games/puzzleGame/puzzleGame.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Draggable, {DraggableCore} from 'react-draggable';
 
-
+//  Assets
 import topLeft from '../../../assets/images/puzzle9x9/pieces/topLeft.png';
 import topCenter from '../../../assets/images/puzzle9x9/pieces/topCenter.png';
 import topRight from '../../../assets/images/puzzle9x9/pieces/topRight.png'; 
@@ -11,59 +11,116 @@ import centerRight from '../../../assets/images/puzzle9x9/pieces/centerRight.png
 import bottomLeft from '../../../assets/images/puzzle9x9/pieces/bottomLeft.png';
 import bottomCenter from '../../../assets/images/puzzle9x9/pieces/bottomCenter.png';
 import bottomRight from '../../../assets/images/puzzle9x9/pieces/bottomRight.png';
-
 import gameImage from '../../../assets/images/puzzle9x9/images/demo.jpg';
+//  Style
 import './puzzleGame.css';
+
+//  Components
+import EndedGameAllert from '../../reusfullComponents/endedGameAllert/endedGameAllert';
+
 export default function PuzzleGame() {
-    const piecesArr = [
-        {piece:topLeft,position:"0% 0%",height:"100px",width:"100px",top:0,left:0},
-        {piece:topCenter,position:"41% 0%",height:"100px",width:"130px",top:0,left:30},
-        {piece:topRight,position:"100% 0%",height:"100px",width:"130px",top:0,left:30},
-        {piece:centerLeft,position:"0% 41%",height:"130px",width:"130px",top:30,left:0},
-        {piece:center,position:"59% 41%",height:"130px",width:"130px",top:30,left:30},
-        {piece:centerRight,position:"100% 50%",height:"160px",width:"100px",top:30,left:30},
-        {piece:bottomLeft,position:"0% 100%",height:"130px",width:"100px",top:60,left:0},
-        {piece:bottomCenter,position:"50% 100%",height:"130px",width:"160px",top:60,left:30},
-        {piece:bottomRight,position:"100% 100%",height:"100px",width:"100px",top:30,left:30}
-    ];
+    const [piecesArr, setPiecesArr] = useState([
+            {found:false,piece:topLeft,position:"0% 0%",height:"100px",width:"100px",top:0,left:0},
+            {found:false,piece:topCenter,position:"41% 0%",height:"100px",width:"130px",top:0,left:30},
+            {found:false,piece:topRight,position:"100% 0%",height:"100px",width:"130px",top:0,left:30},
+            {found:false,piece:centerLeft,position:"0% 41%",height:"130px",width:"130px",top:30,left:0},
+            {found:false,piece:center,position:"59% 41%",height:"130px",width:"130px",top:30,left:30},
+            {found:false,piece:centerRight,position:"100% 50%",height:"160px",width:"100px",top:30,left:30},
+            {found:false,piece:bottomLeft,position:"0% 100%",height:"130px",width:"100px",top:60,left:0},
+            {found:false,piece:bottomCenter,position:"50% 100%",height:"130px",width:"160px",top:60,left:30},
+            {found:false,piece:bottomRight,position:"100% 100%",height:"100px",width:"100px",top:30,left:30}
+        ]);
+    const [gameDone,setGameDone] = useState(null);
+
+    const piecesRefs = [useRef(),useRef(),useRef(),useRef(),useRef(),useRef(),useRef(),useRef(),useRef()];
+
+    const checkPiece = (event,i) => {
+        const tx = piecesRefs[i].current.getBoundingClientRect().left;
+        const ty = piecesRefs[i].current.getBoundingClientRect().top;
+        if ((event.clientX > tx - 70 && event.clientX < tx + 70)
+         && (event.clientY > ty - 70 && event.clientY < ty + 70)) {
+            setTimeout(()=>{
+                let newPiecesArr = [...piecesArr];
+                piecesArr[i].found = true;
+                setPiecesArr(newPiecesArr);
+            },500);
+        }
+    }
+
+    const checkWining = () => {
+        let won = true;
+        for (let i = 0; i < 9; i++) {  
+            if (!piecesArr[i].found){
+                won = false; 
+                break; 
+            }
+        }
+        if (won) setGameDone(<EndedGameAllert xp={200} message={"GOOD GAME"} />)
+    }
+
+    useEffect(()=>checkWining(),[piecesArr]);
   return (
-    <div className='PuzzleGame'>
-        <div className='game'>
-        <div className="slice-image">
-        {piecesArr && piecesArr.map((p,i)=>(
-            <Draggable>
-        <div className='slice' key={i}
-        // className={`${i % 3 != 0 ? "take-left " : "take-space "}${i > 2 && i < 6 ? "take-top" : (i > 5 ? "take-double-top":"")}`}
-        style={{
-            WebkitMaskImage:`url(${p.piece})`,
-            WebkitMaskRepeat:'no-repeat',
-            maskImage:`url(${p.piece})`,
-            maskRepeat:'no-repeat',
-            marginLeft:`${-p.left}px`,
-            marginTop:`${-p.top}px`
-            // top:`${p.top}px`,
-            // left:`${p.left}px`
-            }} >
-                <div 
-                    style={{
-                        backgroundImage:`url(${gameImage})`,
-                        height:p.height,
-                        width:p.width,
-                        backgroundPosition:p.position
-                        }}>
+        
+        <div className='PuzzleGame'>
+            {gameDone && gameDone}
+            <div className="slice-images">
+                {piecesArr && piecesArr.map((p,i)=>(
+                    <Draggable 
+                        key={i}
+                        onDrag={(e)=>checkPiece(e,i)}>
+
+                        <div className='slice' 
+                        style={{
+                            opacity:(p.found ? 0 : 1),
+                            WebkitMaskImage:`url(${p.piece})`,
+                            WebkitMaskRepeat:'no-repeat',
+                            maskImage:`url(${p.piece})`,
+                            maskRepeat:'no-repeat',
+                            zIndex:'3',
+                            }} >
+                                <div 
+                                    style={{
+                                        backgroundImage:`url(${gameImage})`,
+                                        height:p.height,
+                                        width:p.width,
+                                        backgroundPosition:p.position
+                                        }}>
+                                </div>
+                        </div> 
+                </Draggable>
+                ))}
+            </div>
+
+            <div className='full-image'>
+            
+                <div className='slice-image'>
+                    {piecesRefs.map((ref,i)=>
+                        <div 
+                        ref={ref}
+                        className='sliced' 
+                        style={{
+                            opacity:(piecesArr[i].found ? 1 : 0),
+                            WebkitMaskImage:`url(${piecesArr[i].piece})`,
+                            WebkitMaskRepeat:'no-repeat',
+                            maskImage:`url(${piecesArr[i].piece})`,
+                            maskRepeat:'no-repeat',
+                            marginLeft:`${-piecesArr[i].left}px`,
+                            marginTop:`${-piecesArr[i].top}px`,
+                            zIndex:'2',
+                            }} >
+                                <div 
+                                    style={{
+                                        backgroundImage:`url(${gameImage})`,
+                                        height:piecesArr[i].height,
+                                        width:piecesArr[i].width,
+                                        backgroundPosition:piecesArr[i].position
+                                        }}>
+                                </div>
+                        </div> 
+                    )}
                 </div>
-        </div> 
-        </Draggable>
-        ))}
+            </div>
         </div>
-        <div className='full-image'>
-        <img src={gameImage} alt="" />
-        </div>
-        </div>
-        <div className='board'></div>
-
-
-    </div>
   )
 }
 
